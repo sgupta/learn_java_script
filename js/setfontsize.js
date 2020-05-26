b363350450a3540cc50a6d15a789e28532883519
Subhash <sg1973@gmail.com>
2020-05-26 10:02:03 -0400

    Added more examples
    New examples from other lesson

---
+++ js/setfontsize.js
@@ -0,0 +1,10 @@
+function setFont45() {
+   let fsize = "45px";
+   document.getElementById('mytext').style.fontSize = fsize;
+ }
+ function setFont35() {
+   let fsize = "35px";
+   document.getElementById('mytext').style.fontSize = fsize;
+ }
+       
+      
